ad42317904ff697bf1d8837bf538fb7bc1a1fd4b
katuha2024 <ukrbrend2020@gmail.com>
2025-05-11 07:10:24 +0300

    fix reviws

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -14,9 +14,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
     breakpoints: {
       1200: {
-        slidesPerView: 3,
+        slidesPerView: 3.1,
         spaceBetween: 24,
       },
     },
   });
-});
+
+  // Переміщуємо вибір кнопок усередину DOM-завантаження
+  const prevBtn = document.querySelector('.reviews-arrows-prev');
+  const nextBtn = document.querySelector('.reviews-arrows-next');
+
+  function activateButton(button) {
+    button.classList.add('active');
+    setTimeout(() => {
+      button.classList.remove('active');
+    }, 300);
+  }
+
+  prevBtn.addEventListener('click', () => activateButton(prevBtn));
+  nextBtn.addEventListener('click', () => activateButton(nextBtn));
+}); // Тут правильно завершуємо основну функцію
